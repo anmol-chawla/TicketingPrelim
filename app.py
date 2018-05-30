5357bd56a8c9900c4262b53625a9de56ab441b1d
Anmol Chawla <anmolchawla10@gmail.com>
2018-05-30 18:53:21 +0530

    Add a front end

--- app.py
+++ app.py
@@ -1,80 +1,83 @@
-from flask import Flask, request, jsonify, g, url_for
+from flask import Flask, request, jsonify, g, url_for, render_template, redirect, session
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
-from flask_httpauth import HTTPBasicAuth
 from passlib.apps import custom_app_context as pwd_context
 from model import insert_data, check_user
 from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://anmol:pass@localhost/aadb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://anmol:golmol@localhost/aadb'
 db = SQLAlchemy(app)
-auth = HTTPBasicAuth()
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'auth'
 
-    username = db.Column(db.String, primary_key=True)
-    password = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password_hash = db.Column(db.String)
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
 
     def verify_password(self, password):
-        return check_user(self.username, password)
-
-    def generate_auth_token(self, expiration=600):
-        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
-        return s.dumps({'id': self.id})
-
-    @staticmethod
-    def verify_auth_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
-        try:
-            data = s.loads(token)
-        except SignatureExpired:
-            return None  # valid token, but expired
-        except BadSignature:
-            return None  # invalid token
-        user = User.query.get(data['id'])
-        return user
+        return pwd_context.verify(password, self.password_hash)
 
 
-@auth.verify_password
-def verify_password(username_or_token, password):
-    user = User.verify_auth_token(username_or_token)
-    if not user:
-        user = User.query.filter_by(username=username_or_token).first()
-        if not user or not user.verify_password(password):
-            return False
-    g.user = user
-    return True
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
-@app.route('/api/users', methods=['POST'])
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/api/addusers', methods=['POST'])
 def new_user():
     if request.method == "POST":
-        username = request.form.get('username')
-        password = request.form.get('password')
-        if username == '' or password == '':
-            exit(400)  # missing arguments
-        # if User.query.filter_by(username=username).first() != '':
-        #     exit(400)  # existing user
-        user = User(username=username, password=password)
-        if not user.verify_password(password):
-            exit(400)
+        username = request.form['username']
+        password = request.form['password']
+        if username is None or password is None:
+            return jsonify({'status': "Empty form"})  # missing arguments
+        if User.query.filter_by(username=username).first() is not None:
+            return jsonify({'status': 'exists'})  # existing user
+        user = User(username=username)
+        user.hash_password(password)
         db.session.add(user)
         db.session.commit()
         return jsonify({'status': "Success"})
 
 
-@app.route('/api/token')
-@auth.login_required
-def get_auth_token():
-    token = g.user.generate_auth_token(600)
-    return jsonify({'token': token.decode('ascii'), 'duration': 600})
+@app.route("/api/login", methods=['POST'])
+def authorize():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        if username is None or password is None:
+            return jsonify({'status': "Empty form"})  # missing arguments
+        if User.query.filter_by(username=username).first() is not None:
+            user = User.query.filter_by(username=username).first()
+            if user.verify_password(password):
+                g.user = user
+                login_user(user)
+                return redirect(url_for('do_sale'), code=307)
 
 
 @app.route("/sale/record", methods=["POST"])
+@login_required
 def do_sale():
+    return render_template("form_sale.html")
+
+
+@app.route("/api/enter", methods=["POST"])
+@login_required
+def enter_data():
     if request.method == "POST":
         try:
             id = request.form.get('id')
@@ -85,19 +88,14 @@ def do_sale():
             college = request.form.get('college')
             pay_mode = request.form.get('pay_mode')
             type = request.form.get('type')
+            location = request.form.get('location')
             pch = request.form.get('pch')
-            insert_data(id, name, reg_no, mail_id, phone, college, pay_mode, type, pch)
+            insert_data(id, name, reg_no, mail_id, phone, college, pay_mode, location, type, pch)
             return "Success"
         except Exception as e:
             return jsonify({
                 'error': str(e)
             })
-
-
-@app.route('/api/resource')
-@auth.login_required
-def get_resource():
-    return jsonify({'data': 'Hello, %s!' % g.user.username})
 
 
 if __name__ == '__main__':
