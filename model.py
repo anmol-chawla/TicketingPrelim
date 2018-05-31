02c123a7aa760f4282a8f8c97c4e764a40c17579
Anmol Chawla <anmolchawla10@gmail.com>
2018-05-31 22:19:23 +0530

    Update the server access

--- model.py
+++ model.py
@@ -1,21 +1,23 @@
 import pymysql.cursors
 
 connection = pymysql.connect(host='localhost',
-                                  user='anmol',
-                                  password='golmol',
-                                  db='aadb',
-                                  charset='utf8mb4',
-                                  cursorclass=pymysql.cursors.DictCursor)
+                             user='anmol',
+                             password='golmol',
+                             db='aadb',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
 
-def insert_data(identity, name, reg, mail, phone, college, pay, location , event, pch_name):
+def insert_data(identity, name, reg, mail, phone, college, pay, location, event, pch_name):
     try:
         with connection.cursor() as cursor:
             sql = "INSERT INTO Sales VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-            cursor.execute(sql, (int(identity), name, reg, mail, int(phone), college, pay, location, event, pch_name))
+            cursor.execute(sql, (identity, name, reg, mail, phone, college, pay, location, event, pch_name))
             connection.commit()
     except Exception as e:
-        return  e
+        return e
+    finally:
+        return True
 
 
 def check_user(name, pass_string):
